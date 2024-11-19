8a934204193a189012900751abb4a65ea023ffa5
Neilplay <llagasneilandrew@gmail.com>
2024-11-19 13:22:12 +0800

    Update tailwind.config.js

--- aora-main/tailwind.config.js
+++ aora-main/tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
       colors: {
         primary: "#161622",
         secondary: {
-          DEFAULT: "#FF9C01",
-          100: "#FF9001",
-          200: "#FF8E01",
+          DEFAULT: "#FFFFFA",
+          100: "#F7F7F2",
+          200: "#EFEFEA",
         },
         black: {
           DEFAULT: "#000",
